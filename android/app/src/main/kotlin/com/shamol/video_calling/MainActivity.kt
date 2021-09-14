e7e4c6fcdf2dbc25bcf2f194cf5d57a600dc6c36
SHAMOL <shamoldas1997@gmail.com>
2021-09-14 11:49:29 +0600

    Video Calling

---
+++ android/app/src/main/kotlin/com/shamol/video_calling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shamol.video_calling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/settings.dart
@@ -0,0 +1,2 @@
+const APP_ID = 'bdb7e33120fd4bacb8bf43d8cacbda01';
+const Token = '006dcf6d81d2db1463082e45c4fda366eefIAAt7ak+vm/1l8AzUmLCNo6XFjG9ucUtsHXFqBvW+oQ5w6Lzs2wAAAAAEADkBngOT4RBYQEAAQBNhEFh';
